d759dfa5fb5647a9980d38ebd0f6ac23e42c5a44
Jonathan Winters <wintersjonathan0@gmail.com>
2025-11-29 00:39:51 -0800

    fix launch file
    Signed-off-by: Jonathan Winters <wintersjonathan0@gmail.com>

--- test/runTest.ts
+++ test/runTest.ts
@@ -6,11 +6,11 @@ async function main() {
     try {
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
-        const extensionDevelopmentPath = __dirname;
+        const extensionDevelopmentPath = path.resolve(__dirname, '..');
 
         // The path to the extension test runner script
         // Passed to --extensionTestsPath
-        const extensionTestsPath = path.resolve(__dirname, 'test');
+        const extensionTestsPath = path.resolve(__dirname, './index');
 
         // Download VS Code, unzip it and run the integration test
         await runTests({ extensionDevelopmentPath, extensionTestsPath });
